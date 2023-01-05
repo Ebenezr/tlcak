5c4ef03f554eccf3b39e8b560c21836555cca97a
Ebenezr <ebenezarbukosia@gmail.com>
2023-01-05 10:12:01 +0300

    add menu bar to all ipad and tablet screen

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -121,44 +121,46 @@ const Navigation = () => {
             Join Us
           </button>
         </div>
-        <div className="lg:hidden">
-          <button
-            onClick={toggleMenu}
-            className="transition duration-150 ease-in-out z-50"
-          >
-            {isOpen ? (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                className="w-6 h-6 transition duration-150 ease-in-out"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-                />
-              </svg>
-            ) : (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                className="w-6 h-6 transition duration-150 ease-in-out "
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            )}
-          </button>
-        </div>
+        {isMobile ? (
+          <div>
+            <button
+              onClick={toggleMenu}
+              className="transition duration-150 ease-in-out z-50"
+            >
+              {isOpen ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  className="w-6 h-6 transition duration-150 ease-in-out"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  className="w-6 h-6 transition duration-150 ease-in-out "
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              )}
+            </button>
+          </div>
+        ) : null}
       </nav>
       {/* mobile overlay */}
       {!isOpen ? (
