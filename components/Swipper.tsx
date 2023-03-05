6042ef27766e6c5e65f4993d4cadbddbd075ead7
Ebenezr <ebenezarbukosia@gmail.com>
2023-03-05 19:36:59 +0300

    [FIX] minor fixes

--- components/Swipper.tsx
+++ components/Swipper.tsx
@@ -1,17 +1,24 @@
 import React, { useRef, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination, Navigation } from "swiper";
+import { Pagination, Navigation, Autoplay, Thumbs, FreeMode } from "swiper";
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
+import "swiper/css/thumbs";
+import "swiper/css/free-mode";
+
 import Image from "next/image";
+
+import styles from "../styles/Home.module.css";
 
 interface ImageObject {
   src: string;
   alt: string;
 }
+interface SwiperProps {}
 const Swipper = () => {
+  const [thumbsSwiper, setThumbsSwiper] = useState();
   const images: ImageObject[] = [
     { src: "/church.jpg", alt: "church" },
     { src: "/hospitality.png", alt: "hospitality" },
@@ -31,13 +38,21 @@ const Swipper = () => {
     { src: "/10.png", alt: "church" },
   ];
 
+  const handleSwiper = (swiper: any) => {
+    setThumbsSwiper(swiper);
+  };
+
   return (
     <section className=" bg-white" id="gallery">
-      <div className="mx-auto w-full lg:h-[45rem] h-[22rem] md:mb-12 px-4  md:max-w-2xl lg:max-w-6xl mb-6 py-10">
+      <div className="mx-auto w-full lg:h-auto  md:h-auto h-[28rem] md:mb-12 px-4  md:max-w-2xl lg:max-w-6xl mb-6 py-10">
         <h3 className="font-sans font-regular tracking-widest text-center text-lg text-neutral-400 mb-12">
           CHURCH GALLERY
         </h3>
         <Swiper
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
           slidesPerView={1}
           spaceBetween={30}
           loop={true}
@@ -45,224 +60,60 @@ const Swipper = () => {
             clickable: true,
           }}
           navigation={true}
-          modules={[Pagination, Navigation]}
-          className="mySwiper"
+          thumbs={{ swiper: thumbsSwiper }}
+          modules={[Pagination, Navigation, Autoplay, Thumbs]}
+          className={`mySwiper2 ${styles.swiper}`}
         >
-          <SwiperSlide className="rounded-md overflow-hidden">
-            <div className={cardStyle}>
-              <Image
-                src="/church.jpg"
-                alt="church"
-                // placeholder="blur"
-                className="object-center object-cover imgShadow shadow-inner h-full  w-full h-full "
-                layout="fill"
-                objectFit="cover"
-              />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide className="rounded-md overflow-hidden">
-            <div className={cardStyle}>
-              <Image
-                src="/hospitality.png"
-                alt="church"
-                // placeholder="blur"
-                className="object-center object-cover imgShadow shadow-inner h-full  w-full h-full "
-                layout="fill"
-                objectFit="cover"
-              />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide className="rounded-md overflow-hidden">
-            <div className={cardStyle}>
-              <Image
-                src="/pastoral.png"
-                alt="church"
-                // placeholder="blur"
-                className="object-center object-cover imgShadow shadow-inner h-full  w-full h-full "
-                layout="fill"
-                objectFit="cover"
-              />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide className="rounded-md overflow-hidden">
-            <div className={cardStyle}>
-              <Image
-                src="/praise.png"
-                alt="church"
-                // placeholder="blur"
-                className="object-center object-cover imgShadow shadow-inner h-full  w-full h-full "
-                layout="fill"
-                objectFit="cover"
-              />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide className="rounded-md overflow-hidden">
-            <div className={cardStyle}>
-              <Image
-                src="/school.png"
-                alt="church"
-                // placeholder="blur"
-                className="object-center object-cover imgShadow shadow-inner h-full  w-full h-full "
-                layout="fill"
-                objectFit="cover"
-              />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide className="rounded-md overflow-hidden">
-            <div className={cardStyle}>
-              <Image
-                src="/men.png"
-                alt="church"
-                // placeholder="blur"
-                className="object-center object-cover imgShadow shadow-inner h-full  w-full h-full "
-                layout="fill"
-                objectFit="cover"
-              />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide className="rounded-md overflow-hidden">
-            <div className={cardStyle}>
-              <Image
-                src="/1.png"
-                alt="church"
-                // placeholder="blur"
-                className="object-center object-cover imgShadow shadow-inner h-full  w-full h-full "
-                layout="fill"
-                objectFit="cover"
-              />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide className="rounded-md overflow-hidden">
-            <div className={cardStyle}>
-              <Image
-                src="/2.png"
-                alt="church"
-                // placeholder="blur"
-                className="object-center object-cover imgShadow shadow-inner h-full  w-full h-full "
-                layout="fill"
-                objectFit="cover"
-              />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide className="rounded-md overflow-hidden">
-            <div className={cardStyle}>
-              <Image
-                src="/3.png"
-                alt="church"
-                // placeholder="blur"
-                className="object-center object-cover imgShadow shadow-inner h-full  w-full h-full "
-                layout="fill"
-                objectFit="cover"
-              />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide className="rounded-md overflow-hidden">
-            <div className={cardStyle}>
-              <Image
-                src="/4.png"
-                alt="church"
-                // placeholder="blur"
-                className="object-center object-cover imgShadow shadow-inner h-full  w-full h-full "
-                layout="fill"
-                objectFit="cover"
-              />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide className="rounded-md overflow-hidden">
-            <div className={cardStyle}>
-              <Image
-                src="/5.png"
-                alt="church"
-                // placeholder="blur"
-                className="object-center object-cover imgShadow shadow-inner h-full  w-full h-full "
-                layout="fill"
-                objectFit="cover"
-              />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide className="rounded-md overflow-hidden">
-            <div className={cardStyle}>
-              <Image
-                src="/6.png"
-                alt="church"
-                // placeholder="blur"
-                className="object-center object-cover imgShadow shadow-inner h-full  w-full h-full "
-                layout="fill"
-                objectFit="cover"
-              />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide className="rounded-md overflow-hidden">
-            <div className={cardStyle}>
-              <Image
-                src="/7.png"
-                alt="church"
-                // placeholder="blur"
-                className="object-center object-cover imgShadow shadow-inner h-full  w-full h-full "
-                layout="fill"
-                objectFit="cover"
-              />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide className="rounded-md overflow-hidden">
-            <div className={cardStyle}>
-              <Image
-                src="/8.png"
-                alt="church"
-                // placeholder="blur"
-                className="object-center object-cover imgShadow shadow-inner h-full  w-full h-full "
-                layout="fill"
-                objectFit="cover"
-              />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide className="rounded-md overflow-hidden">
-            <div className={cardStyle}>
-              <Image
-                src="/9.png"
-                alt="church"
-                // placeholder="blur"
-                className="object-center object-cover imgShadow shadow-inner h-full  w-full h-full "
-                layout="fill"
-                objectFit="cover"
-              />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide className="rounded-md overflow-hidden">
-            <div className={cardStyle}>
-              <Image
-                src="/10.png"
-                alt="church"
-                // placeholder="blur"
-                className="object-center object-cover imgShadow shadow-inner h-full  w-full h-full "
-                layout="fill"
-                objectFit="cover"
-              />
-            </div>
-          </SwiperSlide>
+          {images.length > 0
+            ? images.map((slide) => (
+                <SwiperSlide
+                  key={slide.src}
+                  className="rounded-md overflow-hidden"
+                >
+                  <div className={cardStyle}>
+                    <Image
+                      src={slide.src}
+                      alt={slide.alt}
+                      // placeholder="blur"
+                      className="object-center object-cover imgShadow shadow-inner h-full  w-full h-full "
+                      layout="fill"
+                      objectFit="cover"
+                    />
+                  </div>
+                </SwiperSlide>
+              ))
+            : null}
+        </Swiper>
+        <Swiper
+          // onSwiper={handleSwiper}
+          spaceBetween={10}
+          slidesPerView={8}
+          freeMode={true}
+          watchSlidesProgress={true}
+          modules={[FreeMode, Navigation, Thumbs]}
+          className="mySwiper mt-3"
+        >
+          {images.length > 0
+            ? images.map((slide) => (
+                <SwiperSlide key={slide.src}>
+                  <div className="h-8 lg:h-12 w-24">
+                    <Image
+                      src={slide.src}
+                      alt={slide.alt}
+                      // placeholder="blur"
+                      className="object-top object-contain imgShadow shadow-inner h-full  w-full h-full "
+                      layout="fill"
+                      objectFit="cover"
+                    />
+                  </div>
+                </SwiperSlide>
+              ))
+            : null}
         </Swiper>
       </div>
     </section>
   );
 };
 export default Swipper;
-
-//   {
-//     images.map((slide) => (
-//       <div key={slide.src}>
-//         yoo
-//         <SwiperSlide>
-//           <Image
-//             src={slide.src}
-//             alt={slide.alt}
-//             // placeholder="blur"
-//             className="object-center object-cover imgShadow shadow-inner h-full  w-full h-full "
-//             layout="fill"
-//             objectFit="cover"
-//           />
-//         </SwiperSlide>
-//       </div>
-//     ));
-//   }
 
 const cardStyle = "h-56 lg:h-[30rem] md:h-[20rem] w-full  bg-white";
