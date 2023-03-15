1c534c7a2a1a7136ea823dbf35365cfacc969d0c
Ebenezr <ebenezarbukosia@gmail.com>
2023-03-15 14:21:54 +0300

    [REF] ref meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,76 +1,76 @@
-import Layout from "../components/Layout";
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import spectral from "@next/font/local";
-import open_sans from "@next/font/local";
-import Head from "next/head";
-import { NextSeo } from "next-seo";
+import Layout from '../components/Layout';
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import spectral from '@next/font/local';
+import open_sans from '@next/font/local';
+import Head from 'next/head';
+import { NextSeo } from 'next-seo';
 
 const Spectral = spectral({
   src: [
     {
-      path: "../assets/Spectral/Spectral-Light.ttf",
-      weight: "200",
-      style: "normal",
+      path: '../assets/Spectral/Spectral-Light.ttf',
+      weight: '200',
+      style: 'normal',
     },
     {
-      path: "../assets/Spectral/Spectral-Regular.ttf",
-      weight: "300",
-      style: "normal",
+      path: '../assets/Spectral/Spectral-Regular.ttf',
+      weight: '300',
+      style: 'normal',
     },
     {
-      path: "../assets/Spectral/Spectral-Medium.ttf",
-      weight: "400",
-      style: "normal",
+      path: '../assets/Spectral/Spectral-Medium.ttf',
+      weight: '400',
+      style: 'normal',
     },
     {
-      path: "../assets/Spectral/Spectral-SemiBold.ttf",
-      weight: "500",
-      style: "normal",
+      path: '../assets/Spectral/Spectral-SemiBold.ttf',
+      weight: '500',
+      style: 'normal',
     },
     {
-      path: "../assets/Spectral/Spectral-Bold.ttf",
-      weight: "700",
-      style: "normal",
+      path: '../assets/Spectral/Spectral-Bold.ttf',
+      weight: '700',
+      style: 'normal',
     },
   ],
-  variable: "--font-spectral",
+  variable: '--font-spectral',
 });
 
 const OpenSans = open_sans({
   src: [
     {
-      path: "../assets/OpenSans/OpenSans-Light.ttf",
-      weight: "200",
-      style: "normal",
+      path: '../assets/OpenSans/OpenSans-Light.ttf',
+      weight: '200',
+      style: 'normal',
     },
     {
-      path: "../assets/OpenSans/OpenSans-Regular.ttf",
-      weight: "300",
-      style: "normal",
+      path: '../assets/OpenSans/OpenSans-Regular.ttf',
+      weight: '300',
+      style: 'normal',
     },
     {
-      path: "../assets/OpenSans/OpenSans-Medium.ttf",
-      weight: "400",
-      style: "normal",
+      path: '../assets/OpenSans/OpenSans-Medium.ttf',
+      weight: '400',
+      style: 'normal',
     },
     {
-      path: "../assets/OpenSans/OpenSans-SemiBold.ttf",
-      weight: "700",
-      style: "normal",
+      path: '../assets/OpenSans/OpenSans-SemiBold.ttf',
+      weight: '700',
+      style: 'normal',
     },
     {
-      path: "../assets/OpenSans/OpenSans-Bold.ttf",
-      weight: "800",
-      style: "normal",
+      path: '../assets/OpenSans/OpenSans-Bold.ttf',
+      weight: '800',
+      style: 'normal',
     },
     {
-      path: "../assets/OpenSans/OpenSans-ExtraBold.ttf",
-      weight: "900",
-      style: "normal",
+      path: '../assets/OpenSans/OpenSans-ExtraBold.ttf',
+      weight: '900',
+      style: 'normal',
     },
   ],
-  variable: "--font-sans",
+  variable: '--font-sans',
 });
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -83,18 +83,18 @@ export default function App({ Component, pageProps }: AppProps) {
         title="True Light Christian assembly Kenya"
         description="True Light Christian Assembly Kenya, founded in 2016, aims to restore lives with God's Word and reach the world through missions and church planting."
         openGraph={{
-          type: "website",
-          siteName: "True Light Christian assembly Kenya",
-          url: "www.truelightchristianassemblykenya.co.ke",
-          title: "True Light Christian assembly Kenya",
+          type: 'website',
+          siteName: 'True Light Christian assembly Kenya',
+          url: 'www.truelightchristianassemblykenya.co.ke',
+          title: 'True Light Christian assembly Kenya',
           description:
             "True Light Christian Assembly Kenya, founded in 2016, aims to restore lives with God's Word and reach the world through missions and church planting.",
           images: [
             {
-              url: "/prev.png",
+              url: '/prev.png',
               width: 1200,
               height: 650,
-              alt: "TLCAK",
+              alt: 'TLCAK',
             },
           ],
         }}
@@ -104,7 +104,7 @@ export default function App({ Component, pageProps }: AppProps) {
           noimageindex: true,
           noarchive: true,
           maxSnippet: -1,
-          maxImagePreview: "none",
+          maxImagePreview: 'none',
           maxVideoPreview: -1,
         }}
       />
@@ -113,7 +113,10 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="https://res.cloudinary.com/dbkeoqmg5/image/upload/v1678872868/backdrop_hohfmq.png" />
+        <meta
+          property="og:image"
+          content="https://res.cloudinary.com/dbkeoqmg5/image/upload/v1678879245/ogimage_ttf5zp.jpg"
+        />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
         <meta property="og:image:alt" content="TLCAK" />
@@ -131,7 +134,10 @@ export default function App({ Component, pageProps }: AppProps) {
         />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
-        <meta property="og:image" content="https://res.cloudinary.com/dbkeoqmg5/image/upload/v1678872868/backdrop_hohfmq.png"></meta>
+        <meta
+          property="og:image"
+          content="https://res.cloudinary.com/dbkeoqmg5/image/upload/v1678879245/ogimage_ttf5zp.jpg"
+        ></meta>
 
         {/* <!-- Twitter --> */}
         <meta property="twitter:card" content="summary_large_image" />
@@ -146,7 +152,10 @@ export default function App({ Component, pageProps }: AppProps) {
         />
         <meta property="twitter:image:width" content="1200" />
         <meta property="twitter:image:height" content="630" />
-        <meta property="twitter:image" content="https://res.cloudinary.com/dbkeoqmg5/image/upload/v1678872868/backdrop_hohfmq.png" />
+        <meta
+          property="twitter:image"
+          content="https://res.cloudinary.com/dbkeoqmg5/image/upload/v1678879245/ogimage_ttf5zp.jpg"
+        />
 
         <title>True Light Christian assembly Kenya</title>
       </Head>
@@ -156,4 +165,3 @@ export default function App({ Component, pageProps }: AppProps) {
     </main>
   );
 }
-
